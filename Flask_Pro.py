2e88ad5132161d1989680a2b74772e10aed39efe
Unknown <pottsbrendan@rocketmail.com>
2017-12-07 14:41:48 +0000

    Initial commit

---
+++ Flask_Pro.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, json
+from flask.ext.mysql import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+@app.route('/signUp', methods=['POST'])
+def signUp():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+    cursor.callproc('sp_createUser', (_name, _email, _password))
+
+    data = cursor.fetchall()
+
+    if len(data) is 0:
+        conn.commit()
+        return json.dumps({'message': 'User created successfully !'})
+    else:
+        return json.dumps({'error': str(data[0])})
+
+if __name__ == '__main__':
+    app.run()
